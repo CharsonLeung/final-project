db65ed012a76c63e268bbda674dea46685599736
charson.leung <charsonleung673@gmail.com>
2025-09-18 20:22:58 +0800

    update

--- project-stock-data/src/main/java/project_stock_data/project_stock_data/entity/StockOhlcEntity.java
+++ project-stock-data/src/main/java/project_stock_data/project_stock_data/entity/StockOhlcEntity.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -20,22 +22,29 @@ import lombok.NoArgsConstructor;
 public class StockOhlcEntity {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private String symbol;
+  @Column(nullable = false, name = "current_price")
   private Float c;
-  @Column(nullable = false, name = "Change")
+  @Column(nullable = false, name = "change")
   private Float d;
-  @Column(nullable = false, name = "Percent_change")
+  @Column(nullable = false, name = "percent_change")
   private Float dp;
-  @Column(nullable = false, name = "High_price_of_the_day")
+  @Column(nullable = false, name = "high_price_of_the_day")
   private Float h;
-  @Column(nullable = false, name = "Low_price_of_the_day")
+  @Column(nullable = false, name = "low_price_of_the_day")
   private Float l;
-  @Column(nullable = false, name = "Open_price_of_the_day")
+  @Column(nullable = false, name = "open_price_of_the_day")
   private Float o;
-  @Column(nullable = false, name = "Previous_close_price")
+  @Column(nullable = false, name = "previous_close_price")
   private Float pc;
-  @Column(nullable = false, name = "Unix")
+  @Column(nullable = false, name = "unix")
   private Integer t;
+  @Column(nullable = false, name = "volume")
+  private Integer v;
 
-  
+  @ManyToOne
+  @JoinColumn(name = "stock_symbol", nullable = false)
+  private StockEntity stockEntity;
+
   
 }
